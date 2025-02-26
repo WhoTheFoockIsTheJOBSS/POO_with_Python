8193964d28501be3639cae3e90af43f71a6ac946
WhoTheFoockIsTheJOBSS <jdha2hotmail.com@gmail.com>
2025-02-26 15:09:12 -0600

    Add initial implementation of classes and objects in Python

---
+++ Clases y objetos.py
@@ -0,0 +1,22 @@
+#sneik_case: el_nombre_se_divide_con_guiones_bajos
+# camelCase: elNombreSeDivideConMayusculas
+# PascalCase: ElNombreSeDivideConMayusculasPeroLaPrimeraTambien
+
+#Nota: Las clases se nombran usando PascalCase
+#Nota: Las funciones se nombran usando camelCase
+
+#Ejemplo de una clase
+class NombreClase(): 
+    propiedade1 = "valor1"
+    propiedade2 = "valor2"
+    propiedade3 = "valor3"
+
+class Celular(): #Clase
+    marca = "Samsung" #Atributo = Propiedades Fijas de la clase 
+    modelo = "Galaxy S10"
+    camaraF = "12MP"
+
+cellular1 = Celular() #Objeto = inta
+print(cellular1.marca)
+print(cellular1.modelo)
+print(cellular1.camaraF)
