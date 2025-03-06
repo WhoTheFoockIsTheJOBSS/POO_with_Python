778658f7835199497b70b2194df8ba96789af79c
WhoTheFoockIsTheJOBSS <jdha2hotmail.com@gmail.com>
2025-03-06 13:18:08 -0600

    Add abstract class Persona and implementations for Estudiante and Trabajador with activity methods

--- Abstraccion/ClasesAbstractas.py
+++ Abstraccion/ClasesAbstractas.py
@@ -1,6 +1,9 @@
+#Las clases abtractas son clases que no se pueden instanciar, es decir, no se pueden crear objetos de ellas.
+#Estas clases se utilizan para definir una estructura base que será heredada por otras clases, y que estas clases hijass
+
 from abc import ABC, abstractmethod
 
-class Persona(ABC):
+class Persona(ABC): # esta clase es la plantilla base para las clases hijas
     @abstractmethod
     def __init__(self, nombre, edad, sexo, actividad):
         self.nombre = nombre
@@ -9,22 +12,35 @@ class Persona(ABC):
         self.actividad = actividad
 
     @abstractmethod
-    def trabajar(self):
+    def hacerActividad(self):
         pass
 
     def presentarse(self):
         print(f"Hola, me llamo {self.nombre} y tengo {self.edad} años")
 
 
-dalto = Persona("Dalto", 25, "Masculino")
-dalto.presentarse() # arroja error porque no se puede instanciar un objeto de una clase abstracta
+# dalto = Persona("Dalto", 25, "Masculino")
+# dalto.presentarse() # arroja error porque no se puede instanciar un objeto de una clase abstracta
 #En este caso, la clase Persona es una clase abstracta, por lo que no se puede instanciar un objeto de esta clase.
 
 class Estudiante(Persona):
     def __init__(self, nombre, edad, sexo, actividad):
         super().__init__(nombre, edad, sexo, actividad)
+        
+    def hacerActividad(self):
+        print(f"Estoy estudiando para ser {self.actividad}")
 
-    def trabajar(self):
-        print(f"Estoy estudiando {self.actividad}")
+class Trabajador(Persona):
+    def __init__(self, nombre, edad, sexo, actividad):
+        super().__init__(nombre, edad, sexo, actividad)
 
-dalto = Estudiante("Dalto", 25, "Masculino", "Programador")
+    def hacerActividad(self):
+        print(f"Actualmente estoy trabajando en un puesto de {self.actividad}")
+
+pedro = Estudiante("Pedro", 25, "Masculino", "Programador")
+dalto = Trabajador("Dalto", 28, "Masculino", "Programador")
+
+dalto.presentarse()
+dalto.hacerActividad()
+pedro.presentarse()
+pedro.hacerActividad()
